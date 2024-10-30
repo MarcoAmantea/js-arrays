13dfcae97e503584dded2a83d73bb7ab9102eadb
MarcoAmantea <marcoamantea18@gmail.com>
2024-10-30 15:44:59 +0100

    feat: exercises 0 to 9

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,34 +10,71 @@ const teachers = [
 
 // 0. Per ogni insegnante stampa in console la frase:
 // [Nome insengante] è l'insegnante in posizione [i]
+for (let i = 0 ; i < teachers.length ; i++){
+  const teachersName = teachers [i]
+  console.log(`${teachersName} è l'insegnante in posizione ${i}`);
+  
+}
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+
+const fourthTeacher = teachers[3];
+console.log(fourthTeacher);
+
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers [4] = `Patrick`
+console.log(teachers);
+
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastItem = teachers.pop()
+const lastTeacher = lastItem;
+console.log(teachers);
+console.log(lastTeacher);
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstItem = teachers.shift()
+const firstTeacher = firstItem;
+console.log(teachers);
+console.log(firstTeacher);
+
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push(`Vanessa`)
+console.log(teachers);
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift(`Sarah`)
+console.log(teachers);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const verify = teachers.includes ("Fabio")
+const isFabioPresent = verify
+console.log(isFabioPresent);
+
+
+
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const indexTeacher = teachers.indexOf ("Lewis")
+const lewisIndex = indexTeacher;
+console.log(lewisIndex);
+
+
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersCode = teachers.join ()
+const teachersString = teachersCode;
+console.log(teachersString);
+
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
